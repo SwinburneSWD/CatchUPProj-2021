2f25498ce940684b40eb13a089fc714c8d8473d4
Anh Nguyen <anguyen@swin.edu.au>
2021-10-08 11:39:12 +1100

    added sql

--- HeadphoneLib/Headphones.cs
+++ HeadphoneLib/Headphones.cs
@@ -5,6 +5,6 @@ namespace HeadphoneLib
     public class Headphone {
         public int Id { get; set; }
         public string DriverType { get; set; }
-        public string Catergory { get; set; }
+        public string Category { get; set; }
     }
 }
---
+++ HeadphoneSQL/dml.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Headphone (
+    [id] INT PRIMARY KEY,
+    driverType VARCHAR(50),
+    category VARCHAR(50) 
+);
+
+
+INSERT INTO Headphone ([id], driverType, category) VALUES (1, 'Dynamic', 'closed');
+INSERT INTO Headphone ([id], driverType, category) VALUES (2, 'Ortho', 'open-ear');
+INSERT INTO Headphone ([id], driverType, category) VALUES (3, 'BA', 'in-ear');
+INSERT INTO Headphone ([id], driverType, category) VALUES (4, 'ElectroStat', 'open-ear');
